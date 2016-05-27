6f67ebd680348e94494a949a239d96cc90af04f3
furaoing <furaoing@hotmail.com>
2016-05-27 17:06:51 +0800

    updates

--- waffle/tmp/dynamic_programming/fibonaci.py
+++ waffle/tmp/dynamic_programming/fibonaci.py
@@ -3,17 +3,20 @@
 
 def fibo(n):
     if n == 0:
-        return 0
+        print("Invalid Input")
+        raise Exception
     if n == 1:
         return 1
     if n == 2:
         return 1
-    fibo_series = [1, 1]
-
-    for i in range(2, n+1):
-        added = fibo_series[i-1] + fibo_series[i-2]
-        fibo_series.append(added)
-
-    return fibo_series[-1]
+    else:
+        fibo_series = [1, 1]
+        k = n - 1
+        for i in range(2, n):
+            added = fibo_series[i-1] + fibo_series[i-2]
+            fibo_series.append(added)
+            # fibo_series[i] = added
+        print(fibo_series)
+        return fibo_series[-1]
 
 print(fibo(6))
