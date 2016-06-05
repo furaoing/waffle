f9589a08cb212fd99ec320d25dcfc9358fa2acd4
furaoing <furaoing@hotmail.com>
2016-06-05 20:28:09 +0800

    updates

---
+++ waffle/math/combinations.py
@@ -0,0 +1,38 @@
+def expand_element(x, y, depth):
+    code = []
+    ax = x[depth]
+    for i in ax:
+        code.append(y + [i])
+    return code
+
+
+def expand_group(x, y, depth):
+    code = []
+    for a_list in y:
+        code = code + expand_element(x, a_list, depth)
+    return code
+
+
+def helper(depth, bank):
+    if depth == 0:
+        tmp = bank[depth]
+        ax = []
+        for x in tmp:
+            ax.append([x])
+        return ax
+    else:
+        expanded = helper(depth-1, bank)
+        return expand_group(bank, expanded, depth)
+
+
+def generate_comb(bank):
+    width = len(bank)
+    depth = width - 1
+    result = helper(depth, bank)
+    print(len(result))
+    print(result)
+
+
+if __name__ == "__main__":
+    bank = [['a', 'b'], ['c', 'd']]
+    generate_comb(bank)
